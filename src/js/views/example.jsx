e5fa1c1a1a7ffc92ba51ad3d5bc05befdb2041f8
behughes <behughes@microsoft.com>
2014-11-28 23:40:52 +0000

    Forgot to add example jsx fle

---
+++ src/js/views/example.jsx
@@ -0,0 +1,14 @@
+"use strict";
+var React = require('react');
+
+module.exports = React.createClass({
+    render: function() {
+        return (
+            <div className="commentList">
+
+            Hello, world! I am a Test. How are you today? I'm OK! Thanks!
+
+            </div>
+        );
+    }
+});
