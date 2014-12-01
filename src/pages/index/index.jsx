e6557aabb4332d34f31b781b62dee35e1030e0b5
behughes <behughes@microsoft.com>
2014-12-01 01:04:59 +0000

    Adding basic styling to home page

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -18,11 +18,14 @@ module.exports = React.createClass({
     },
 
     render: function() {
+        var language = lang("en");
         return (
             <div className="page userList">
-                <h1>Welcome please input your list id</h1>
-                <h2>If you're new just type in a unique name (something you'll remember) and press submit</h2>
-                <input name="listId" placeholder="unique list name" value={this.state.listId} onSubmit={this.onSubmit} onChange={this.handleListIdChange}/><a href="#" onClick={this.onSubmit}>Submit</a>
+                <div className="messageHolder">
+                    <h1>{language.pageTitle}</h1>
+                    <p><strong>{language.messagePart1}</strong>{language.messagePart2}</p>
+                    <input name="listId" placeholder="unique list name" value={this.state.listId} onSubmit={this.onSubmit} onChange={this.handleListIdChange}/><a href="#" onClick={this.onSubmit}>Submit</a>
+                </div>
             </div>
         );
     }
--- src/pages/index/lang.js
+++ src/pages/index/lang.js
@@ -2,7 +2,9 @@
 
 var lang = {
     "en": {
-        "pageTitle": "Moving List"
+        "pageTitle": "Welcome! please input your list ID",
+        "messagePart1": "First time here?",
+        "messagePart2": "Just type in a unique name (something you'll remember) and press submit to start creating your list"
     },
     "fr": {
 
